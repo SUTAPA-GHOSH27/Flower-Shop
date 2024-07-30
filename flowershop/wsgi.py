2042d7a61bf3567ed57a4356ae812d91c77baf21
SUTAPA GHOSH <sutapaghosh0405@gmail.com>
2024-07-31 03:22:51 +0530

    last sample change

--- flowershop/wsgi.py
+++ flowershop/wsgi.py
@@ -8,11 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowershop.settings')
 
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 application = get_wsgi_application()
 
-app= application
+app = application
